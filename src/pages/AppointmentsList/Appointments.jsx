fc709763d0df2e96b9de5bff0f2aa4e6c12b93ac
mdmhrz <razufreelence@gmail.com>
2025-06-02 14:06:16 +0600

    My booking cancel appointment updated

--- src/pages/AppointmentsList/Appointments.jsx
+++ src/pages/AppointmentsList/Appointments.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const Appointments = () => {
+const Appointments = ({ lawyersData, handleCancelAppointments }) => {
+    // console.log(lawyersData);
+    const { name, expertise, fee, id } = lawyersData;
     return (
-        <div>
-            Hello ami appoinments
+        <div className='border-2 border-gray-200 rounded-xl p-5 mb-8'>
+            <div className='flex justify-between mb-4'>
+                <div>
+                    <h2 className='font-bold text-xl'>{name}</h2>
+                    <p>{expertise}</p>
+                </div>
+                <div>
+                    <p className='bg-green-200 py-1 px-4 rounded-2xl text-green-800 font-medium'>Appointment Fee: BDT {fee}</p>
+                </div>
+            </div>
+            <button onClick={() => handleCancelAppointments(id)} className='btn btn-error btn-outline rounded-3xl w-full'>Cancel Appointment</button>
         </div>
     );
 };
--- src/pages/MyBookings/MyBookings.jsx
+++ src/pages/MyBookings/MyBookings.jsx
@@ -7,6 +7,26 @@ const MyBookings = () => {
 
     const [appointmentList, setAppointmentList] = useState([]);
 
+    const handleCancelAppointments = (id) => {
+
+        const data = JSON.parse(localStorage.getItem('appointmentList'));
+        // console.log(data);
+        const updatedData = data.filter((itemId) => itemId.id !== id)
+        console.log(updatedData);
+        console.log(id);
+
+        localStorage.setItem('appointmentList', JSON.stringify(updatedData));
+
+        // const appointmentsData = getBookedAppointment();
+        // localStorage.removeItem()
+        const remainingAppointments = appointmentList.filter((clicked) => clicked.id !== id);
+        setAppointmentList(remainingAppointments);
+
+
+
+
+    }
+
     const data = useLoaderData();
 
     useEffect(() => {
@@ -18,14 +38,14 @@ const MyBookings = () => {
 
 
     return (
-        <div>
-            <div>
-                <h1>My Today Appointment</h1>
+        <div className='max-w-11/12 mx-auto'>
+            <div className='text-center mb-8 p-10'>
+                <h1 className='font-bold text-2xl'>My Today Appointment</h1>
                 <p>Our platform connects you with verified, experienced Lawyers across various specialties — all at your convenience.</p>
             </div>
             <div>
                 {
-                    appointmentList.map((lawyersData, index) => <Appointments key={index} lawyersData={lawyersData}></Appointments>)
+                    appointmentList.map((lawyersData, index) => <Appointments key={index} lawyersData={lawyersData} handleCancelAppointments={handleCancelAppointments}></Appointments>)
                 }
             </div>
         </div>
