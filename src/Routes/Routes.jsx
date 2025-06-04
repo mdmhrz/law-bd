a21c8c10fcaf8ec20b9134e14caf7ce9b9835f9d
mdmhrz <razufreelance@gmail.com>
2025-06-05 02:18:39 +0600

    Suspense Added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,5 +1,6 @@
 import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom"; // use "react-router-dom" not "react-router"
+import GlobalSpinner from "../components/GlobalSpinner/GlobalSpinner";
 
 const Home = lazy(() => import("../pages/Home/Home"));
 const MyBookings = lazy(() => import("../pages/MyBookings/MyBookings"));
@@ -7,7 +8,7 @@ const Blogs = lazy(() => import("../pages/Blogs/Blogs"));
 const LawyerDetails = lazy(() => import("../pages/LawyerDetails/LawyerDetails"));
 const Root = lazy(() => import("../pages/Root/Root"));
 const ErrorPage = lazy(() => import("../pages/ErrorPage/ErrorPage"));
-import LoadingSpinner from "../components/LoadingSpinner/loadingSpinner";
+
 
 
 export const router = createBrowserRouter([
@@ -20,30 +21,26 @@ export const router = createBrowserRouter([
                 index: true,
                 loader: () => fetch('/lawyerData.json').then(res => res.json()),
                 path: '/',
-                element: <Suspense fallback={<div><span className="loading loading-ball loading-xs"></span>
-                    <span className="loading loading-ball loading-sm"></span>
-                    <span className="loading loading-ball loading-md"></span>
-                    <span className="loading loading-ball loading-lg"></span>
-                    <span className="loading loading-ball loading-xl"></span></div>}>
+                element: <Suspense fallback={<GlobalSpinner></GlobalSpinner>}>
                     <Home></Home>
                 </Suspense>
             },
             {
                 path: '/myBookings',
                 loader: () => fetch('/lawyerData.json').then(res => res.json()),
-                element: <Suspense fallback={LoadingSpinner}>
+                element: <Suspense fallback={<GlobalSpinner></GlobalSpinner>}>
                     <MyBookings></MyBookings>
                 </Suspense>
             },
             {
                 path: '/blogs',
                 loader: () => fetch('/blog.json'),
-                Component: Blogs
+                element: <Suspense fallback={<GlobalSpinner></GlobalSpinner>}><Blogs></Blogs></Suspense>
             },
             {
                 path: '/lawyerDetails/:id',
                 loader: () => fetch('/lawyerData.json').then(res => res.json()),
-                Component: LawyerDetails
+                element: <Suspense fallback={<GlobalSpinner></GlobalSpinner>}><LawyerDetails></LawyerDetails></Suspense>
             }
         ]
     },
